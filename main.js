34a21c77b99e99487b8990a5ea1f658aef91f073
Javidu-sudo <77806108+Javidu-sudo@users.noreply.github.com>
2021-09-07 17:47:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,22 @@ function capture(){
     })
 }
 console.log("ml5"+ml5.version)
-classafyer = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/yfUv2hogT/",modleLoded)
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/yfUv2hogT/",modleLoded)
 function modleLoded(){
     console.log("loded status loded")
 }
+function identify(){
+    img = document.getElementById("capture_image")
+    classifier.classify(img, got_result)
+}
+function got_result(error, results){
+    if (error){
+        console.error(error)
+    }
+    else{
+        console.log(results)
+        console.error('ERRROROROROROROROROOOOOORRRORROROROR')
+        document.getElementById("result-object").innerHTML = results[0].label
+        document.getElementById('result-accuracy').innerHTML = results[0].confidence.toFixed(3)
+    }
+    }
